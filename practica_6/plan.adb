b11916d5afc2406840e95ac3e3ea8d1ec6c7c76f
Julio-AV <julio.alvarezv@edu.uah.es>
2024-04-27 19:48:18 +0200

    add planificar and prueba

--- practica_6/plan.adb
+++ practica_6/plan.adb
@@ -15,4 +15,26 @@ package body plan is
             Tiempos(i) := Natural(to_duration(t_diff)*1000);
         end loop;
     end medir;
+
+    procedure Planificar (Tareas: in out array_reg_Planificacion_t) is 
+        
+        aux : reg_Planificacion_t;
+    begin 
+        --ordenamos las listas con bubble sort
+        for i in Tareas'Range loop
+            for j in Tareas'First .. Tareas'Last-1 loop
+                if (Tareas(j).D > Tareas(j+1).D) or ((Tareas(j).D = Tareas(j+1).D) and (Tareas(j).T > Tareas(j+1).T))  then
+                    aux := Tareas(J);
+                    Tareas(j) := Tareas(j+1);
+                    Tareas(j+1) := aux;
+               
+
+                end if;
+            end loop;
+        end loop;
+
+
+    end Planificar;
+
 end plan;
+
--- practica_6/plan.ads
+++ practica_6/plan.ads
@@ -4,6 +4,17 @@ package plan is
     type array_ref_Procedimiento_t is array (Positive range <>)
     of ref_Procedimiento_t;
     type array_Tiempos_t is array (Positive range <>) of Natural;
+    type reg_Planificacion_t is record
+        Nombre : Positive; -- Número de la tarea
+        T : Natural; -- Período
+        D : Natural; -- Plazo
+        C : Natural; -- Tiempo de cómputo
+        P : Positive; -- Prioridad
+        R : Natural; -- Tiempo de respuesta
+        Planificable: Boolean;
+    end record;
+    type array_reg_Planificacion_t is array (Positive range <>) of reg_Planificacion_t;
+    procedure Planificar (Tareas: in out array_reg_Planificacion_t);
     procedure Medir (Procedimientos: array_ref_Procedimiento_t;
     Tiempos : out array_Tiempos_t);
 end plan;
