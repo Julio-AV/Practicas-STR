a9aff1f59bbc180f9ff822ee8c6ac741fe99dd02
Julio-AV <julio.alvarezv@edu.uah.es>
2024-04-30 11:51:59 +0200

    complete simular

--- practica_6/simular.adb
+++ practica_6/simular.adb
@@ -5,28 +5,43 @@ with proc; use proc;
 procedure simular is 
 
 type ref_Procedimiento_t is access procedure;
-type array_ref_Procedimiento_t is array (Positive range <>) of ref_Procedimiento_t;
-type array_ejecutivo_t is array (Positive range <>) of array_ref_Procedimiento_t;
-task type Tarea_t (Nombre : Natural;
+--type array_ref_Procedimiento_t is array (Positive range <>) of ref_Procedimiento_t;
+task type Tarea_t  (
+    Nombre : Natural;
     T : Natural;
     D : Natural;
     C : Natural;
     P : Natural;
-    Codigo_Ciclico: ref_Procedimiento) is pragma Priority (P);
+    Codigo_Ciclico: ref_Procedimiento_t) is pragma Priority (P);
 end Tarea_t;
+
 Tarea1: Tarea_t (1, 2400, 600, 400, 4, P1'Access);
 Tarea2: Tarea_t (2, 3200, 1200, 600, 3, P2'Access);
 Tarea3: Tarea_t (3, 3600, 2000, 800, 2, P3'Access);
 Tarea4: Tarea_t (4, 4000, 3200, 800, 1, P4'Access);
-t_start : time;
-t_ini_proceso : time;
-periodo_basico : Natural := 2400;
-array_ciclo_ejecutivo : array_ejecutivo_t := ((Tarea1, Tarea2, Tarea3, null),
-                                                    (Tarea1));
-begin   
-    while Integer(to_duration(clock - t_start)) < 50000 loop
-        for i in array_ciclo_ejecutivo'Range loop
 
-        end loop;
+task body Tarea_t is 
+multiplicador : Natural := 1;
+t_transcurrido : Integer;
+t_ini : Time;
+t_start : Time;
+begin 
+    t_start := clock;
+    t_ini := clock;
+    while Integer(to_duration(clock - t_start)) < 50000 loop
+        Codigo_Ciclico.all;
+        t_transcurrido := Integer(To_Duration(Clock - T_Ini)*1000);
+        if t_transcurrido > D then --No se ha ejecutado en el deadline
+            put_Line("La tarea: " & Nombre'Image & "NO se ha ejecutado en el plazo");
+        else 
+            put_Line("La tarea: " & Nombre'Image & " se ha ejecutado en el plazo");
+        end if;
+        T_Ini := T_Ini + Milliseconds(T);
+        delay until t_ini;
+        multiplicador := multiplicador+1;
     end loop;
+end Tarea_t;
+
+begin 
+    null;
 end simular;
