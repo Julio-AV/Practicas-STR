17152183d6c5284d47113334ed2228a00ca03a01
Julio-AV <julio.alvarezv@edu.uah.es>
2024-05-01 12:41:15 +0200

    solve 50s timeout

--- practica_6/simular.adb
+++ practica_6/simular.adb
@@ -19,7 +19,7 @@ Tarea1: Tarea_t (1, 2400, 600, 400, 4, P1'Access);
 Tarea2: Tarea_t (2, 3200, 1200, 600, 3, P2'Access);
 Tarea3: Tarea_t (3, 3600, 2000, 800, 2, P3'Access);
 Tarea4: Tarea_t (4, 4000, 3200, 800, 1, P4'Access);
-
+ext : Boolean := False;
 task body Tarea_t is 
 multiplicador : Natural := 1;
 t_transcurrido : Integer;
@@ -28,7 +28,7 @@ t_start : Time;
 begin 
     t_start := clock;
     t_ini := clock;
-    while Integer(to_duration(clock - t_start)) < 50000 loop
+    while not ext  loop
         Codigo_Ciclico.all;
         t_transcurrido := Integer(To_Duration(Clock - T_Ini)*1000);
         if t_transcurrido > D then --No se ha ejecutado en el deadline
@@ -43,5 +43,7 @@ begin
 end Tarea_t;
 
 begin 
-    null;
+    delay 50.0;
+    ext := True;
+    put_Line("Fin controlado de la simulación");
 end simular;
